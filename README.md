3f5843067aa8ab9e79928c17da9b9cc9cb23a374
mundusnine <mundusnine@gmail.com>
2023-12-05 13:10:22 -0500

    Use a dll of fenster instead, calc is a POC

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-## Dependencies
-
-wget cosmocc 3.1.3 and add the directory cosmocc at the root of this workspace.
-
-## Building
-
-To build the dll and the executable, simply call knob.com.
-
-To understand the build system just look at the knob.c file.
+## Cosmo Utilities
+Smol single binary cross-platform apps.
--- knob.c
+++ knob.c
@@ -1,23 +1,37 @@
 #define KNOB_IMPLEMENTATION
 #include "knob.h"
-
+extern const int __hostos;
 #include <cosmo.h>
 
 MAIN(test_build){
-
-    if(!knob_mkdir_if_not_exists("./Tests")){
-        knob_log(KNOB_ERROR,"Failed creating Test folder. Something is really wrong...");
+    
+    if(!knob_mkdir_if_not_exists("./Deployment")){
+        knob_log(KNOB_ERROR,"Failed creating Deployment folder. Something is really wrong...");
         return 1;
     }
     Knob_Cmd cmd = {0};
     //Build .so
-    knob_cmd_append(&cmd,"cc","-ggdb3","-shared","-o","./Tests/libdll.so","-fPIC","dll.c");
+    //For dll do:
+    // # macOS
+    // -framework Cocoa -framework AudioToolbox
+    // # windows
+    // -lgdi32 -lwinmm
+    knob_cmd_append(&cmd,"cc","-I./Libraries/fenster","-ggdb3","-shared","-o","./Deployment/fenster.so","-fPIC","dll.c");
+    if(IsLinux()){
+        knob_cmd_append(&cmd,"-lX11","-lasound");
+    }
+    else if(IsWindows()){
+        knob_cmd_append(&cmd,"-lgdi32","-lwinmm");
+    }
+    else if(IsXnu()){
+        knob_cmd_append(&cmd,"-framework","Cocoa","-framework","AudioToolbox");
+    }
     if(!knob_cmd_run_sync(cmd)){
         knob_log(KNOB_ERROR,"Failed build of library.");
         return 1;
     }
     cmd.count = 0;
-    knob_cmd_append(&cmd,"./cosmocc/bin/cosmocc","-g","-o","loader.com","libraryloader.c");
+    knob_cmd_append(&cmd,"./cosmocc/bin/cosmocc","-I./Libraries/fenster","-g","-o","./Deployment/calc.com","calc.c");
     if(!knob_cmd_run_sync(cmd)){
         knob_log(KNOB_ERROR,"Failed build of executable.");
         return 1;
